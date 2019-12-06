cb0fa34a364590229a1d5894300fca8a5b7d5b70
joyvalley <jan.freudenthal@uni-wuerzburg.de>
2019-12-06 22:45:23 +0100

    good night

--- gblup.r
+++ gblup.r
@@ -4,5 +4,5 @@ tMM  <- t(M) %*% (M)
 mafs = c(0.3,0.2,0.1,0.15)
 P = matrix(rep(2*(mafs - 0.5),3),nrow=3,byrow=T)
 Z = M - P
-G = (Z %*% t(Z)) /  (2 * sum(mafs*(1-mafs) ))
+G = (Z %*% t(Z)) /  (2 * sum(mafs*(1-mafs)
 G
